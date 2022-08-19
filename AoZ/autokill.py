bb4b823c601305ae38eee51e871f31846ab440d0
Néstor Coppi <nestorcoppi@gmail.com>
2022-08-19 12:38:26 -0300

    Improving Autokill

--- AoZ/autokill.py
+++ AoZ/autokill.py
@@ -19,18 +19,13 @@ import os
 import re
 import sys
 import time
+from datetime import datetime
 
 from com.dtmilano.android.viewclient import ViewClient, KEY_EVENT
 
 def puts(msg):
     print(str(datetime.now().strftime("[%d/%m/%Y %H:%M:%S] ")) + msg)
 
-# TAG = 'CULEBRA'
-# _s = 5
-# _v = '--verbose' in sys.argv
-# pid = os.getpid()
-
-from datetime import datetime
 def now():
     # Getting the current date and time
     dt = datetime.now()
@@ -48,10 +43,12 @@ class AutoKill():
         self.vc = ViewClient(self.device, self.serialno, **kwargs2)
 
         self.ataques = 0
-        # self.tiempo = now()
+        # argv will only be used for fuel from the moment
+        self.use_fuel = (len(sys.argv) == 1)
+        # self.time = now()
 
     def autokill(self):
-        puts("Atacando... Nº " + str(self.ataques))
+        puts("Attacking... Nº " + str(self.ataques))
         # Estando en la ciudad vamos al mapa
         self.click_en_casa_o_mapa()
 
@@ -62,14 +59,13 @@ class AutoKill():
         self.click_en_casa_o_mapa()
 
         # Esperar a que llegue el ataque
-        # self.vc.sleep(300)
-        self.esperar_y_ayudar(300)
+        self.wait_and_help(self.attack_time, 'attack')
 
         self.curar_y_esperar()
 
-        self.ayudar()
+        self.help()
 
-        if (self.necesita_combustible()):
+        if self.use_fuel:
             self.cargar_combustible()
 
         self.ataques += 1
@@ -78,7 +74,7 @@ class AutoKill():
         self.autokill()
 
     def atacar(self):
-        puts("Atacando...")
+        puts("Attacking...")
         ## Aca boton verde
         self.device.touchDip(380.19, 582.86, 0)
 
@@ -87,7 +83,7 @@ class AutoKill():
         ## Confirmar busqueda
         self.device.touchDip(367.24, 678.1, 0)
 
-        self.vc.sleep(4)
+        self.vc.sleep(3)
 
         # Click en bicho
         self.device.touchDip(209.52, 335.24, 0)
@@ -97,8 +93,7 @@ class AutoKill():
 
         # Atacar
         self.device.touchDip(224.76, 486.86, 0)
-
-        self.vc.sleep(3)
+        self.vc.sleep(2)
 
         # Click en formacion 2
         # self.device.touchDip(158.48, 35.81, 0)
@@ -106,10 +101,10 @@ class AutoKill():
         self.vc.sleep(1)
 
         # Confirmar
-        # TODO: Leer tiempo?
+        self.attack_time = self.get_flew_time()
         self.device.touchDip(364.95, 701.71, 0)
-
-        self.vc.sleep(3)
+        self.attacking_until = now() + (self.attack_time * 2)
+        self.vc.sleep(2)
 
     def click_en_casa_o_mapa(self):
         puts("Click en casa o mapa")
@@ -122,36 +117,42 @@ class AutoKill():
         puts("Curando")
         # Click en + hospital
         self.device.touchDip(331.43, 409.14, 0)
+        self.vc.sleep(1)
         self.device.touchDip(331.43, 409.14, 0)
         self.vc.sleep(2)
         # Confirmar
-        # TODO: Leer el tiempo
+        heal_time = self.get_heal_time()
+
+        if (heal_time < self.attack_time):
+            heal_time = self.attack_time
+
         self.device.touchDip(369.52, 695.62, 0)
-        self.vc.sleep(2)
+        self.vc.sleep(1)
         # Pedir ayuda
         self.device.touchDip(331.43, 409.14, 0)
         self.vc.sleep(1)
         self.device.touchDip(331.43, 409.14, 0)
-        # (5:42 minutos)
-        self.esperar_y_ayudar(340)
+        self.wait_and_help(heal_time, 'heal')
         # Sacar los wachines
         self.device.touchDip(331.43, 409.14, 0)
         self.vc.sleep(1)
 
 
     def cargar_combustible(self):
-        puts("Cargando combustible")
+        puts("Checkeando combustible")
         # Click en parte de combustible
         self.device.touchDip(79.24, 45.71, 0)
-
-        self.vc.sleep(1)
-        # Click en 1er usar
-        self.device.touchDip(329.14, 285.71, 0)
         self.vc.sleep(1)
 
-        # Confirmar
-        self.device.touchDip(207.24, 395.43, 0)
-        self.vc.sleep(1)
+        if (self.get_current_fuel() < 20):
+            puts("Cargando combustible...")
+            # Click en 1er usar
+            self.device.touchDip(329.14, 285.71, 0)
+            self.vc.sleep(1)
+
+            # Confirmar
+            self.device.touchDip(207.24, 395.43, 0)
+            self.vc.sleep(1)
 
         # Click fuera del modal
         self.device.touchDip(158.48, 91.43, 0)
@@ -161,9 +162,9 @@ class AutoKill():
         # ahora = now()
 
         # cada 1 hora, skippeamos junto con los ataques
-        # if (ahora - self.tiempo) > 3600 and (self.ataques % 2) == 0:
+        # if (ahora - self.time) > 3600 and (self.ataques % 2) == 0:
         #     puts(str(ahora) + " Pasaron 3 horas... reseteando")
-        #     self.tiempo = ahora
+        #     self.time = ahora
         #     self.ataques = 0
         #     return False
 
@@ -173,13 +174,13 @@ class AutoKill():
 
         # return True
 
-    def ayudar(self):
+    def help(self):
         # Click en Alianza
         self.device.touchDip(321.52, 691.81, 0)
         self.vc.sleep(1)
         # Click en Ayuda
         self.device.touchDip(232.38, 521.14, 0)
-        self.vc.sleep(3)
+        self.vc.sleep(2)
 
         # Click en Ayudar
         self.device.touchDip(219.43, 690.29, 0)
@@ -191,13 +192,79 @@ class AutoKill():
         self.device.touchDip(24.38, 25.14, 0)
         self.vc.sleep(1)
 
-    def esperar_y_ayudar(self, tiempo):
-        puts("Esperando " + str(tiempo) + "s y ayudando")
+    def wait_and_help(self, time, kind):
+        puts("Waiting " + str(time) + "s & helping")
         start = now()
 
-        while (now() - start) < tiempo:
-            self.ayudar()
+        while (now() - start) < time:
+            self.help()
+            if (kind == 'heal' and self.attack_ready() and self.heal_ready()):
+                return
             self.vc.sleep(5)
+
+    def attack_ready(self):
+        return (now() >= self.attacking_until)
+
+    def get_heal_time(self):
+        kwargs1 = {'verbose': False, 'ignoresecuredevice': False, 'ignoreversioncheck': False}
+        device, _serialno = ViewClient.connectToDeviceOrExit(**kwargs1)
+
+        ss = device.takeSnapshot()
+        raw_time = device.imageToData(
+            ss.crop((747,1635,747+178, 1635+84)).convert("L") # cortar el tiempo y monocromear
+        )['text'][-1]
+
+        parts = list(map(lambda n: int(n), raw_time.split(':')))
+
+        if len(parts) == 2:
+            return parts[0] * 60 + parts[1]
+        else:
+            return parts[0]
+
+    def heal_ready(self):
+        kwargs1 = {'verbose': False, 'ignoresecuredevice': False, 'ignoreversioncheck': False}
+        device, _serialno = ViewClient.connectToDeviceOrExit(**kwargs1)
+
+        ss = device.takeSnapshot()
+        results = device.imageToData(
+            ss.crop((704,1210,704+271,1210+157)).convert("L") # cortar el tiempo y monocromear
+        )['text']
+
+        return 'Curacion' not in results
+
+    def get_flew_time(self):
+        kwargs1 = {'verbose': False, 'ignoresecuredevice': False, 'ignoreversioncheck': False}
+        device, _serialno = ViewClient.connectToDeviceOrExit(**kwargs1)
+
+        ss = device.takeSnapshot()
+        raw_time = device.imageToData(
+            ss.crop((698,1698,698+328,1798)).convert("L") # cortar el tiempo y monocromear
+        )['text'][-1]
+
+        parts = list(map(lambda n: int(n), raw_time.split(':')))
+
+        if len(parts) == 2:
+            return parts[0] * 60 + parts[1]
+        else:
+            return parts[0]
+
+    def get_current_fuel(self, retries = 0):
+        if retries > 2:
+            return False
+
+        try:
+            kwargs1 = {'verbose': False, 'ignoresecuredevice': False, 'ignoreversioncheck': False}
+            device, _serialno = ViewClient.connectToDeviceOrExit(**kwargs1)
+
+            ss = device.takeSnapshot()
+            return int(
+                device.imageToData(
+                    ss.crop((633,533,633+130,533+70)).convert("L") # cortar el tiempo y monocromear
+                )['text'][-1]
+            )
+        except:
+            self.vc.sleep(1)
+            return self.get_current_fuel(retries + 1)
 
 
 if __name__ == "__main__":
